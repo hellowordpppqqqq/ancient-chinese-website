b43d4f35d3296e45230becd1c97eaa65d5619c6c
eveningwater <854806732@qq.com>
2025-03-06 10:32:40 +0000

    feat: 添加了新的字

---
+++ .vitepress/components/font-source.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-popover :title="props?.text" trigger="click" width="300">
+        <template #reference>
+            <el-text type="primary" size="large" class="cursor-pointer">{{ props?.text }}</el-text>
+        </template>
+        <div class="pinyin-explain">
+            <div class="pinyin">{{ props?.explain?.pinyin }}</div>
+            <div class="explain" v-for="(item, index) in props?.explain?.explain" :key="`${item}-${index}`">
+                <div>{{ index + 1 }}.{{ item?.explain }}</div>
+                <el-text size="small">内容来源:《{{ item?.source }}》</el-text>
+            </div>
+            <el-link :href="props?.explain?.more" target=“__blank” style="margin-top:10px;">
+                查看更多
+            </el-link>
+        </div>
+    </el-popover>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        default: ''
+    },
+    explain: {
+        type: Object,
+        default: () => ({
+            pinyin: '',
+            explain: [{
+                explain: '',
+                source: ''
+            }],
+            more: ''
+        })
+    }
+})
+</script>
--- .vitepress/theme/index.mts
+++ .vitepress/theme/index.mts
@@ -2,8 +2,11 @@ import ElementPlus from "element-plus";
 import "element-plus/theme-chalk/index.css";
 import DefaultTheme from "vitepress/theme";
 import ReadText from "../components/read-text.vue";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import PreviewPDF from "../components/preview-pdf.vue";
+import FontSource from "../components/font-source.vue";
+import Analysis from "../components/analysis.vue";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
 
 export default {
   extends: DefaultTheme,
@@ -14,5 +17,7 @@ export default {
     }
     app.component("ac-read-text", ReadText);
     app.component("ac-preview-pdf", PreviewPDF);
+    app.component("ac-font-source", FontSource);
+    app.component("ac-analysis", Analysis);
   },
 };
